f3f6ad450414c3857f04ee5a6af7a893568cbfbd
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-04-28 14:37:20 +0400

    first day and JavaScript Drum Kit

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,17 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key = "${e.key}"]`);
+  const keyElement = document.querySelector(`.key[data-key = "${e.key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  keyElement.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((el) => el.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
